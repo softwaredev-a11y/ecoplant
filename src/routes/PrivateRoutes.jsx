6c1e2426e4e08a04c9332118cf83a9c76c9e5059
Edison <softwaredev@totaltrackinggps.com>
2025-09-04 16:35:05 -0500

    feat(routes): User context is added to parent components.

--- src/routes/PrivateRoutes.jsx
+++ src/routes/PrivateRoutes.jsx
@@ -4,20 +4,21 @@ import DashboardIndexPage from "../pages/DashboardIndexPage";
 import PlantDetailsPage from "../pages/PlantDetailsPage";
 import NotFoundPage from "../pages/NotFoundPage";
 import { PlantProvider } from "../context/PlantContext";
+import { UserProvider } from "../context/UserContext";
 
 function PrivateRoutes() {
   return (
     <PlantProvider>
-      <Routes>
-        <Route path="/" element={<DashboardLayout />}>
-          <Route index element={<DashboardIndexPage />} />
-          <Route path="planta/:idPlanta" element={<PlantDetailsPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Route>
-
-        {/* Por si alguien pone mal la URL de dashboard */}
-        <Route path="*" element={<Navigate to="/dashboard" />} />
-      </Routes>
+      <UserProvider>
+        <Routes>
+          <Route path="/" element={<DashboardLayout />}>
+            <Route index element={<DashboardIndexPage />} />
+            <Route path="planta/:idPlanta" element={<PlantDetailsPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Route>
+          <Route path="*" element={<Navigate to="/dashboard" />} />
+        </Routes>
+      </UserProvider>
     </PlantProvider>
   );
 }

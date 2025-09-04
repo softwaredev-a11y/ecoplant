0f2e8d6e0ff57abc86bee71d61fad47b0e9116de
Edison <softwaredev@totaltrackinggps.com>
2025-09-04 16:48:40 -0500

    feat(hooks): New hooks have been added, which allow querying the connection status and executing commands.

--- src/hooks/usePlants.jsx
+++ src/hooks/usePlants.jsx
@@ -1,6 +1,8 @@
 // src/hooks/usePlants.js
-import { useContext } from "react";
+import { useContext, useEffect, useState, useCallback } from "react";
 import { PlantContext } from "../context/PlantContext";
+import { PlantDetailSocketContext } from "../context/PlantDetailSocketContext";
+import plants from '../api/plantsApi'
 
 export const usePlants = () => {
   const context = useContext(PlantContext);
@@ -8,4 +10,70 @@ export const usePlants = () => {
     throw new Error("usePlants debe usarse dentro de un PlantProvider");
   }
   return context;
+};
+
+export const usePlantDetailSocket = () => {
+  const context = useContext(PlantDetailSocketContext);
+  if (!context) {
+    throw new Error("usePlants debe usarse dentro de un PlantProvider");
+  }
+  return context;
+};
+
+export const useConnectionStatus = (imei) => {
+  const [infoConnectionDevice, setInfoConnectionDevice] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    if (!imei) {
+      setLoading(false);
+      return;
+    }
+    const fetchStatus = async () => {
+      setLoading(true);
+      setError(null);
+      try {
+        const response = await plants.getConectionStatus(imei);
+        setInfoConnectionDevice(response.data);
+      } catch (err) {
+        setError("Error al obtener el estado de conexión...");
+        setInfoConnectionDevice(null);
+        console.error(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchStatus();
+  }, [imei]);
+  return { infoConnectionDevice, loading, error };
+};
+
+export const useCommandExecution = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [executedCids, setExecutedCids] = useState([]);
+  const [error, setError] = useState(null);
+  const executeMultipleCommands = useCallback(async (imei, commands) => {
+    try {
+      setIsLoading(true);
+      setError(null);
+      const cids = [];
+
+      for (let i = 0; i < commands.length; i++) {
+        const response = await plants.commandExecution(imei, commands[i]);
+        if (response?.data?.cid) cids.push(response.data.cid);
+        if (i < commands.length - 1) {
+          await new Promise(resolve => setTimeout(resolve, 1000));
+        }
+      }
+      setExecutedCids(cids);
+      return cids;
+    } catch (err) {
+      setError("Error al ejecutar los comandos.");
+      console.error(err);
+      return [];
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
+  return { isLoading, executedCids, error, executeMultipleCommands };
 };

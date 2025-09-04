85938b8aee6e13bb3a4aafa46fcb8754d67f6e07
Edison <softwaredev@totaltrackinggps.com>
2025-09-04 16:08:12 -0500

    feat(api): Methods for interacting with API endpoints are defined.

--- src/api/plantsApi.js
+++ src/api/plantsApi.js
@@ -2,7 +2,9 @@ import axiosInstance from "./axiosInstance";
 
 const plantsApi = {
     getPlants: () => axiosInstance.get('/vehicles'),
-    getPlantsPictures: (idPlant) => axiosInstance.get(`/images/vehicles/${idPlant}/photo`)
+    getPlantsPictures: (idPlant) => axiosInstance.get(`/images/vehicles/${idPlant}/photo`),
+    getConectionStatus: (imei) => axiosInstance.get(`/devices/${imei}`),
+    commandExecution: (idDevice, command) => axiosInstance.post(`/vehicles/${idDevice}/remote/console`, { cmd: command, includeImei: true }),
 };
 
 export default plantsApi;

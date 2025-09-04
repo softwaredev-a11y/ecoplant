c3aaab42393a09bac56e0f631e446a83cec433c4
Edison <softwaredev@totaltrackinggps.com>
2025-09-04 16:34:16 -0500

    feat(context): The context for users is developed.

---
+++ src/context/UserInfoContext.jsx
@@ -0,0 +1,30 @@
+import { createContext, useEffect, useState } from "react";
+import usersApi from "../api/usersApi";
+
+export const UserContext = createContext();
+export const UserProvider = ({ children }) => {
+    const [user, setUser] = useState();
+    const [isSuperUser, setIsSuperUser] = useState();
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+    useEffect(() => {
+        const getInfoUser = async () => {
+            try {
+                setLoading(true);
+                const response = await usersApi.getUser();
+                setUser(response.data);
+                setIsSuperUser(user.last_name.includes('superuser'));
+            } catch (error) {
+                setError(error);
+            } finally {
+                setLoading(false);
+            }
+        }
+        getInfoUser();
+    }, [])
+    return (
+        <UserContext.Provider value={{ isSuperUser, loading, error }}>
+            {children}
+        </UserContext.Provider>
+    );
+}

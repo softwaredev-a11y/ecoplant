ec6f0cc50f35992fc25f48db006869b583a2edc3
Edison <softwaredev@totaltrackinggps.com>
2025-09-08 15:43:29 -0500

    feat(context): Stores the list of plants in cache.

--- src/context/PlantContext.jsx
+++ src/context/PlantContext.jsx
@@ -5,14 +5,20 @@ export const PlantContext = createContext();
 
 export const PlantProvider = ({ children }) => {
   const [plants, setPlants] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const getPlants = async () => {
+      setIsLoading(true);
       try {
-        setIsLoading(true);
-        const response = await plantsApi.getPlants();
-        setPlants(response.data.data);
+        const cachedPlants = sessionStorage.getItem('listPlants');
+        if (cachedPlants) {
+          setPlants(JSON.parse(cachedPlants))
+        } else {
+          const response = await plantsApi.getPlants();
+          setPlants(response.data.data);
+          sessionStorage.setItem('listPlants', JSON.stringify(response.data.data));
+        }
       } catch (error) {
         console.error("Error cargando plantas:", error);
       } finally {

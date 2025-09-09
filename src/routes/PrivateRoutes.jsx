37c1cde4612dd7d87b485dcbf8569c43133f51a8
Edison <softwaredev@totaltrackinggps.com>
2025-09-09 08:20:44 -0500

    feat(routes): Implement route protection

--- src/routes/PrivateRoutes.jsx
+++ src/routes/PrivateRoutes.jsx
@@ -1,25 +1,34 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, Outlet } from "react-router-dom";
 import DashboardLayout from "../layouts/DashboardLayout";
 import DashboardIndexPage from "../pages/DashboardIndexPage";
-import PlantDetailsPage from  "../pages/informationplant/PlantDetailsPage";
+import PlantDetailsPage from "../pages/informationplant/PlantDetailsPage";
 import NotFoundPage from "../pages/NotFoundPage";
 import { PlantProvider } from "../context/PlantContext";
 import { UserProvider } from "../context/UserInfoContext";
+import ProtectedRoute from "./ProtectedRoute";
+
+const ProtectedProvidersLayout = () => (
+  <PlantProvider>
+    <UserProvider>
+      <Outlet />
+    </UserProvider>
+  </PlantProvider>
+);
 
 function PrivateRoutes() {
   return (
-    <PlantProvider>
-      <UserProvider>
-        <Routes>
+    <Routes>
+      <Route element={<ProtectedRoute />}>
+        <Route element={<ProtectedProvidersLayout />}>
           <Route path="/" element={<DashboardLayout />}>
             <Route index element={<DashboardIndexPage />} />
             <Route path="planta/:idPlanta" element={<PlantDetailsPage />} />
             <Route path="*" element={<NotFoundPage />} />
           </Route>
-          <Route path="*" element={<Navigate to="/dashboard" />} />
-        </Routes>
-      </UserProvider>
-    </PlantProvider>
+        </Route>
+      </Route>
+      <Route path="*" element={<Navigate to="/dashboard" />} />
+    </Routes>
   );
 }
 

bafb1aef51e70177dcdfc70e7818068c96b352d4
Edison <softwaredev@totaltrackinggps.com>
2025-09-09 14:10:54 -0500

    feat(plants): Implemented functionality that prevents the socket from connecting if a device is offline. It also prevents commands from being sent to an inactive device.

--- src/context/PlantDetailSocketContext.jsx
+++ src/context/PlantDetailSocketContext.jsx
@@ -7,52 +7,62 @@ export const PlantDetailSocketProvider = ({ children, plantId, isOnline }) => {
     const [lastEvent, setLastEvent] = useState(null);
     const [isConnected, setIsConnected] = useState(false);
     useEffect(() => {
-        if (!plantId || !isOnline) return;
-        const socket = io("https://aws-live-1.pegasusgateway.com/socket", {
-            transports: ["websocket"],
-        });
+        let socket;
 
-        const credentials = {
-            pegasus: "https://rastreo.totaltracking.co",
-            auth: sessionStorage.getItem("token"),
-        };
-
-        socket.on("connect", () => {
-            setIsConnected(true);
-            console.log("Socket conectado");
-            socket.emit("authenticate", credentials);
-        });
-
-        socket.on("_authenticated", () => {
-            console.log("Autenticado en socket");
-            socket.emit("listen", {
-                namespace: "vehicle-events",
-                objects: plantId,
-                compact: true,
+        if (plantId && isOnline) {
+            socket = io("https://aws-live-1.pegasusgateway.com/socket", {
+                transports: ["websocket"],
             });
-        });
 
-        socket.on("events", (envelope) => {
-            setLastEvent(envelope);
-        });
+            const credentials = {
+                pegasus: "https://rastreo.totaltracking.co",
+                auth: sessionStorage.getItem("token"),
+            };
 
-        socket.on("disconnect", () => {
-            setIsConnected(false);
-            console.warn("Socket desconectado");
-        });
+            socket.on("connect", () => {
+                if (!isOnline) return; 
+                setIsConnected(true);
+                console.log("Socket conectado");
+                socket.emit("authenticate", credentials);
+            });
 
-        socket.on("connect_error", (err) => {
-            console.error("Error de conexión:", err.message);
-        });
+            socket.on("_authenticated", () => {
+                console.log("Autenticado en socket");
+                socket.emit("listen", {
+                    namespace: "vehicle-events",
+                    objects: plantId,
+                    compact: true,
+                });
+            });
 
-        socket.on("_error", (message) => {
-            console.error("Error de autenticación:", message);
-        });
+            socket.on("events", (envelope) => {
+                setLastEvent(envelope);
+            });
 
+            socket.on("disconnect", () => {
+                setIsConnected(false);
+                console.warn("Socket desconectado");
+            });
+
+            socket.on("connect_error", (err) => {
+                console.error("Error de conexión:", err.message);
+            });
+
+            socket.on("_error", (message) => {
+                console.error("Error de autenticación:", message);
+            });
+        } else {
+            console.log("Dispositivo offline, no conectar socket");
+        }
         return () => {
-            if (socket.connected || socket.connecting) {
-                console.log("Cerrando socket...");
-                socket.disconnect();
+            if (socket) {
+                socket.removeAllListeners();
+                if (socket.connected || socket.connecting) {
+                    console.log("Cerrando socket...");
+                    socket.disconnect();
+                } else {
+                    console.log("Socket nunca terminó de conectar, no hay nada que cerrar");
+                }
             }
             setIsConnected(false);
         };

6066831bbd9ac2f4854fa5c36c84e7b226b0033e
Edison <softwaredev@totaltrackinggps.com>
2025-09-12 15:40:19 -0500

    docs: add clarifying comments

--- src/utils/plantUtils.jsx
+++ src/utils/plantUtils.jsx
@@ -1,3 +1,8 @@
+/**
+ * Obtiene el modelo de la planta.
+ * @param {String} text Cadena de texto que viene de consultar la api.
+ * @returns {String} Modelo de la planta
+ */
 export function getPlantModel(text) {
     const match = text.match(/\*type:Ecoplant\s*(\d+)/i);
     if (match) {
@@ -6,15 +11,32 @@ export function getPlantModel(text) {
     return 'Sin información disponible';
 }
 
+/**
+ * Busca una planta dentro de una lista
+ * @param {[]} plants Lista de plantas
+ * @param {int} idPlant ID de la planta a buscar
+ * @returns {object} Planta buscada
+ */
 export function searchPlant(plants, idPlant) {
     const plant = plants.find(p => String(p.id) === String(idPlant));
     return plant;
 }
-
+/**
+ * Obtiene el mvZeroValue de una cadena de texto.
+ * @param {String} descripcion Descripción del dispositivo que viene de la consulta de la api.
+ * @returns {String} valor de mv zero.
+ */
 export function getMvZeroText(descripcion) {
     const match = descripcion.match(/mv_zero:(.*?)\nEND_PARAMS/s);
     return match ? match[1] : null;
 }
+
+/**
+ * Función para formatear el tiempo en una cadena de texto.
+ * @param {String} unitTime Unidad de tiempo (horas, segundos, minutos)
+ * @param {int} time Cantidad de tiempo 
+ * @returns {String} Tiempo formateado, ejemplo: 1 segundo, 10 minutos, 1 minutos, 12 horas, etc.
+ */
 
 export function formatTime(unitTime, time) {
     let totalSeconds = getTimeInSeconds(unitTime, time);
@@ -29,7 +51,12 @@ export function formatTime(unitTime, time) {
         ? parts.slice(0, -1).join(" ") + " y " + parts.slice(-1)
         : parts[0];
 }
-
+/**
+ * Función para obtener el tiempo en segundos.
+ * @param {String} unitTime Unidad de tiempo (horas, segundos, minutos)
+ * @param {int} time Cantidad de tiempo 
+ * @returns {int} Tiempo convertido en segundos.
+ */
 export function getTimeInSeconds(unitTime, time) {
     let totalSeconds = 0;
     switch (unitTime.toLowerCase()) {
@@ -48,19 +75,43 @@ export function getTimeInSeconds(unitTime, time) {
     return totalSeconds;
 }
 
+/**
+ * Obtiene la versión de software a partir de la configuración del dispositivo.
+ * @param {String} configuration Configuración del dispositivo.
+ * @returns {String} Versión del software.
+ */
 export function getSoftwareVersion(configuration) {
     const mapConfigurations = { "p458": "1.2", "q111": "1.3", "q149": "1.4", "q537": "2.0", "r058": "3.0 1Horario", "r347": "1.0 AguaFria" };
     return mapConfigurations[configuration] || "0.0";
 }
 
+/**
+ * Contruye una fecha con el siguiente formato: YYYY-MM-DD
+ * @param {String} year año de la fecha a formatear
+ * @param {String} month  mes de la fecha a formatear
+ * @param {String} day día de la fecha a formatear
+ * @returns {String} Fecha formateada.
+ */
 export function buildDate(year, month, day) {
     return `${year}-${fillLeftText(month, 2)}-${fillLeftText(day, 2)}`;
 }
 
+/**
+ * Pone n carácteres a la izquierda de un número
+ * @param {int} num  Número a modificar
+ * @param {int} padlen Cantidad total de números/caracteres que va a tener.
+ * @param {char} padchar Caracter que se va a poner a la izquierda, por defecto está en 0.
+ * @returns {String} Número formateado con 0 al izquierda.
+ */
 export function fillLeftText(num, padlen, padchar = '0') {
     return String(num).padStart(padlen, padchar);
 }
 
+/**
+ * Calcula el valor de la filtración a partir del mensaje que viene de la consula a la api o socket.
+ * @param {String} message Mensaje que viene de una consulta a una api o del servidor.
+ * @returns {int} Valor de la filtración.
+ */
 export function calculateFiltrationValue(message) {
     if (!message) return null;
     const match = message.match(/SGC04TC(\d+);/);
@@ -69,6 +120,11 @@ export function calculateFiltrationValue(message) {
     return rawValue;
 }
 
+/**
+ * Calcula el valor del retrolavado a partir del mensaje que viene de la consula a la api o socket.
+ * @param {String} message Mensaje que viene de una consulta a una api o del servidor.
+ * @returns {int} Valor del retrolavado.
+ */
 export function calculateBackwashValue(message) {
     if (!message) return null;
     const match = message.match(/SGC07TC(\d+);/);
@@ -77,6 +133,11 @@ export function calculateBackwashValue(message) {
     return rawValue;
 }
 
+/**
+ * Calcula el valor del enjuague a partir del mensaje que viene de la consula a la api o socket.
+ * @param {String} message Mensaje que viene de una consulta a una api o del servidor.
+ * @returns {int} Valor del enjuague.
+ */
 export function calculateRinseValue(message) {
     if (!message) return null;
     const match = message.match(/SGC10TC(\d+);/);
@@ -85,6 +146,11 @@ export function calculateRinseValue(message) {
     return rawValue;
 }
 
+/**
+ * Calcula el valor del valor de alerta de flujo insuficiente a partir del mensaje que viene de la consula a la api o socket.
+ * @param {String} message Mensaje que viene de una consulta a una api o del servidor.
+ * @returns {int} Valor de la alerta de flujo insuficiente.
+ */
 export function calculateFlowAlertValue(message, mvZeroValue) {
     if (!message) return null;
     const match = message.match(/RXAGA03V(\d+);/);
@@ -94,6 +160,12 @@ export function calculateFlowAlertValue(message, mvZeroValue) {
     return rawValueToGpm;
 }
 
+/**
+* Calcula el valor del valor de alarma insuficiente a partir del mensaje que viene de la consula a la api o socket.
+ * @param {String} message Mensaje que viene de una consulta a una api o del servidor.
+ * @param {int} mvZeroValue Valor del caudal.
+ * @returns {int} Valor de la alerta de alarma insuficiente.
+ */
 export function calculateInsufficientAlarmValue(message, mvZeroValue) {
     if (!message) return null;
     const match = message.match(/RXAGA00V(\d+);/);
@@ -103,11 +175,22 @@ export function calculateInsufficientAlarmValue(message, mvZeroValue) {
     return rawValueToGpm;
 }
 
+/**
+ * Convierte el valor del voltaje a gpm.
+ * @param {int} voltageValue Valor del voltaje
+ * @param {int} mvZeroValue Valor del caudal.
+ * @returns {int} Valor de GPM.
+ */
 export function conversionToGpm(voltageValue, mvZeroValue) {
     const resultConversion = Math.round((voltageValue - parseInt(mvZeroValue)) / 100);
     return resultConversion;
 }
 
+/**
+ * Obtiene la operación que se está ejecutando de acuerdo al código de estado.
+ * @param {int} statusCode Código de la operación
+ * @returns {String} Operación que se está ejecutando.
+ */
 export function stateProcess(statusCode) {
     const statesByCode = {
         20: "Contacto bomba activado. Esperando...",
@@ -136,6 +219,11 @@ export function stateProcess(statusCode) {
     return `Desconocido: ${statusCode}`;
 }
 
+/**
+ * Determina si debe mostrar o no el valor del flujo actual de acuerdo a la operación que se está ejecutando.
+ * @param {int} statusCode Código de la operación
+ * @returns {boolean}si se debe mostrar o no el flujo actual en el panel de descripción.
+ */
 export function showCurrentFlow(statusCode) {
     const codFiltracion = [4, 6, 7, 8, 17, 19, 65, 66];
     if (codFiltracion.includes(statusCode)) {
@@ -151,10 +239,10 @@ export function showCurrentFlow(statusCode) {
 }
 
 /**
- * 
- * @param {String} message 
- * @param {int} mvZeroValue 
- * @returns 
+ * Obtiene el tipo de operación y su respectivo valor.
+ * @param {String} message Mensaje que viene desde la consulta a la api o desde un evento del socket.
+ * @param {string | null} mvZeroValue Valor del caudal cero (mv_zero).
+ * @returns {{key: string, value: string|number}|null} Un objeto con la clave de la operación y su valor procesado, o `null` si el mensaje no es relevante.
  */
 export function processSocketMessage(message, mvZeroValue) {
     if (!message) {
@@ -197,7 +285,7 @@ export function processSocketMessage(message, mvZeroValue) {
 /**
  * Calcular el valor de flujo actual.
  * @param {int} currentValue valor que viene en el mensaje del websocket o de la consulta a la API.
- * @returns valor del flujo actual.
+ * @returns {int} valor del flujo actual.
  */
 export function calculateStateFlow(currentValue) {
     let flowValue = currentValue / 100;
@@ -209,11 +297,11 @@ export function calculateStateFlow(currentValue) {
 }
 
 /**
- * Obtiene el proceso que se está ejecutando en la planta.
+ * Obtiene el código proceso que se está ejecutando en la planta.
  * @param {String} message Mensaje que viene del websocket por una query.
- * @returns Proceso que se está ejecutando actualmente.
+ * @returns {number|null} Código del proceso que se está ejecutando actualmente, o null si no se encuentra.
  */
-export function getCodeCurrentProccess(message) {
+export function getCodeCurrentProcess(message) {
     if (!message) {
         return null;
     }
@@ -227,9 +315,9 @@ export function getCodeCurrentProccess(message) {
 /**
  * Obtiene el valor del flujo actual de acuerdo a la información que manda el socket.
  * @param {String} message Mensaje que viene del websocket por una query.
- * @returns valor del flujo actual.
+ * @returns {number|string} valor del flujo actual, o una cadena vacía si no se encuentra.
  */
-export function getFlowCurrentlyValue(message) {
+export function getFlowCurrentValue(message) {
     const match = message.match(/AD=(\d+);/);
     if (!match) return "";
     const value = calculateStateFlow(match[1], 10);
@@ -249,9 +337,9 @@ export function thousandsSeparator(num) {
 
 /**
  * Calcula el valor de la filtración.
- * @param {*} caudalValue 
- * @param {*} countFiltered 
- * @returns valor de la filtración.
+ * @param {float} caudalValue 
+ * @param {float} countFiltered 
+ * @returns {float} valor de la filtración.
  */
 export function calculateAccumulatedValueFiltration(caudalValue, countFiltered) {
     return (caudalValue * countFiltered * 2);
@@ -262,7 +350,7 @@ export function calculateAccumulatedValueFiltration(caudalValue, countFiltered) 
  * Calcula el valor del enjuague
  * @param {float} caudalValue 
  * @param {float} countFiltered 
- * @returns valor del enjuague.
+ * @returns  {float}  valor del enjuague.
  */
 export function calculateAccumulatedValueRinse(caudalValue, countFiltered) {
     return caudalValue * countFiltered * 2;
@@ -272,7 +360,7 @@ export function calculateAccumulatedValueRinse(caudalValue, countFiltered) {
  * Calcula el valor del retrolavado
  * @param {float} caudalValue  
  * @param {float} countBackwash 
- * @returns valor del retrolavado
+ * @returns  {float}  valor del retrolavado
  */
 export function calculateAccumulatedValueBackwash(caudalValue, countBackwash) {
     return caudalValue * countBackwash * 3;
@@ -283,7 +371,7 @@ export function calculateAccumulatedValueBackwash(caudalValue, countBackwash) {
  * @param {String} str Cadena a modificar
  * @param {int} index índice del carácter que se va a modificar.
  * @param {String} replacement Cáracter que se va a poner en la cadena.
- * @returns cadena de texto con el nuevo caracter.
+ * @returns {String} cadena de texto con el nuevo caracter.
  */
 export function replaceAt(str, index, replacement) {
     if (index < 0 || index >= str.length) return str; // índice inválido
@@ -294,7 +382,7 @@ export function replaceAt(str, index, replacement) {
  * Realiza la conversión de gpm a voltage.
  * @param {String} gpmValue Valor de gpm en alertaflujo y alarmainsuficiente.
  * @param {String} mvZero valor que hace parte de la información de la planta.
- * @returns valor converitido a voltage.
+ * @returns {int} valor converitido a voltage.
  */
 export function conversionToVoltage(gpmValue, mvZero) {
     const result = (parseInt(gpmValue) * 100) + parseInt(mvZero);
@@ -304,12 +392,13 @@ export function conversionToVoltage(gpmValue, mvZero) {
 
 /**
  * Obtiene el comando set para cambiar los parametros de operación.
- * @param {String} codeOperation  código de la operación (65:filtrado, 32:retrolavado, 12:enjuague, 03:alertaflujo, 00:alarmainsuficiente) 
+ * Construye el string del comando a enviar al dispositivo para actualizar un parámetro.
+ * @param {string} codeOperation  código de la operación (65:filtrado, 32:retrolavado, 12:enjuague, 03:alertaflujo, 00:alarmainsuficiente).
  * @param {boolean} isAlertOperation determina si la operación es por alerta, o alarma.
  * @param {int} value valor ingresado por el usuario.
- * @param {String} unitValue unidades, pueden ser segundos, minutos, horas o gpm.
+ * @param {string} unitValue unidades, pueden ser segundos, minutos, horas o gpm.
  * @param {int} mvZero valor que hace parte de la información de la planta.
- * @returns El comando para realizar la modificación.
+ * @returns {string} El comando formateado para enviar al dispositivo, o una cadena vacía si hay un error.
  */
 export function getSetterMessage(codeOperation, isAlertOperation, value, unitValue, mvZero) {
     const proccess = { "65": "filtrado", "32": "retrolavado", "12": "enjuague", "03": "alertaflujo", "00": "alarmainsuficiente" };
@@ -354,7 +443,7 @@ export function getSetterMessage(codeOperation, isAlertOperation, value, unitVal
 /**
  * Obtiene la cabecera del comando, de acuerdo al código de la operación.
  * @param {String} codeOperation código de la operación (65:filtrado, 32:retrolavado, 12:enjuague, 03:alertaflujo, 00:alarmainsuficiente) 
- * @returns la cabecera del comando.
+ * @returns {String} la cabecera del comando.
  */
 export function getHeaderMessage(codeOperation) {
     const proccess = { "65": "SGC04TC", "32": "SGC07TC", "12": "SGC10TC", "03": "RXAGA03V", "00": "RXAGA00V" };

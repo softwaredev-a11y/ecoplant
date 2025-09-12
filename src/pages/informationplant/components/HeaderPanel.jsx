a17b9e55094724a11f4204a33a4e0ec43792bef4
Edison <softwaredev@totaltrackinggps.com>
2025-09-12 15:42:28 -0500

    docs: add clarifying comments

--- src/pages/informationplant/components/HeaderPanel.jsx
+++ src/pages/informationplant/components/HeaderPanel.jsx
@@ -1,3 +1,9 @@
+/**
+ * Componente que renderiza la cabecera para los paneles de información.
+ * @param {object} props - Las propiedades del componente.
+ * @param {string} props.title - El título que se mostrará en la cabecera.
+ * @returns {JSX.Element} El elemento JSX de la cabecera del panel.
+ */
 export default function HeaderPanel({ title }) {
     return (
         <div className="header-info w-full bg-[#005596] min-h-[30px] flex justify-center items-center tracking-wide">

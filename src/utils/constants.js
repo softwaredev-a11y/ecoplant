7b9bcb2888bbe28d74ccd9078f3354f9a038ce41
Edison <softwaredev@totaltrackinggps.com>
2025-09-12 15:43:30 -0500

    Refactor: Centralize constants in a constants file

---
+++ src/utils/constants.js
@@ -0,0 +1,29 @@
+export const COMMANDS = {
+    FILTRATION: "QED06",
+    BACKWASH: "QED14",
+    RINSE: "QED34",
+    FLOW_ALERT: "QXAGA03",
+    INSUFFICIENT_FLOW_ALARM: "QXAGA00",
+};
+
+export const OPERATION_CODES = {
+    FILTRATION: "65",
+    BACKWASH: "32",
+    RINSE: "12",
+    FLOW_ALERT: "03",
+    INSUFFICIENT_FLOW_ALARM: "00",
+};
+
+export const SOCKET_KEYS = {
+    FILTRATION: "filtrado",
+    BACKWASH: "retrolavado",
+    RINSE: "enjuague",
+    FLOW_ALERT: "valorAlertaFlujo",
+    INSUFFICIENT_FLOW_ALARM: "valorAlarmaInsuficiente",
+};
+
+export const RAW_DATA_CODES = {
+    FILTRATION: 65,
+    RINSE: 32,
+    BACKWASH: 12,
+};

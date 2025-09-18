a5d0cbab1d662e0a1ec308aa4b353402e5f48e19
Edison <softwaredev@totaltrackinggps.com>
2025-09-18 09:50:59 -0500

    feat: add the service to log in to the cloud

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,4 +1,4 @@
-import axiosInstance from "./axiosInstance";
+import { apiPegasusInstance, apiCloudInstance } from "./axiosInstance";
 
 /**
  * Objeto que agrupa las llamadas a la API relacionadas con la autenticación.
@@ -11,12 +11,18 @@ const authApi = {
      * @param {object} data - Los datos para el inicio de sesión, generalmente { username, password }.
      * @returns {Promise} Una promesa que resuelve con la respuesta de la API.
      */
-    login: (data) => axiosInstance.post("/login", data),
+    login: (data) => apiPegasusInstance.post("/login", data),
     /**
      * Realiza una petición GET para cerrar la sesión.
      * @returns {Promise} Una promesa que resuelve con la respuesta de la API.
      */
-    logout: () => axiosInstance.get('/logout')
+    logout: () => apiPegasusInstance.get('/logout'),
+    /**
+     * Realiza una petición POST para iniciar sesión.
+     * @param {object} data - Los datos para el inicio de sesión, generalmente { email, password }.
+     * @returns {Promise} Una promesa que resuelve con la respuesta de la API.
+     */
+    cloudLogin: (data) => apiCloudInstance.post("/auth/login", data)
 };
 
 export default authApi;

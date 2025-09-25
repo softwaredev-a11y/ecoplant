09a7d7aa0136e7e86dc1b46235cdbfd078e9f16c
Edison <softwaredev@totaltrackinggps.com>
2025-09-25 09:46:52 -0500

    doc: agrega documentación

--- src/components/StatusMessage.jsx
+++ src/components/StatusMessage.jsx
@@ -1,4 +1,9 @@
-
+/**
+ * Componente reutilizable para mostrar un mensaje de error.
+ * @param {object} props - Propiedades del componente.
+ * @param {string} props.message - El mensaje de errorr a mostrar.
+ * @returns {JSX.Element} El elemento de que muestra mensaje de error.
+ */
 export default function StatusMessage({ message }) {
     return (
         <p className="text-neutral-600  mb-2">

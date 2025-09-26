4173e7b93e261be90cb4d8aa3b5a3a2faf008914
Edison <softwaredev@totaltrackinggps.com>
2025-09-26 15:40:17 -0500

    feat: agrega instancias al array de dependencias

--- src/hooks/usePlants.js
+++ src/hooks/usePlants.js
@@ -187,7 +187,7 @@ export const useRawDataConsult = () => {
         setIsLoading(false);
       }
     }
-  }, []);
+  }, [setRawData, setIsLoading, setError]);
 
   return { isLoading, rawData, error, rawDataConsult };
 };

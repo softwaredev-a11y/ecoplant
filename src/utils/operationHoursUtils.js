4f6816e4547995456f8a5ac6a6668f8b6ddb8a62
Edison <softwaredev@totaltrackinggps.com>
2025-10-04 08:31:16 -0500

    feat: elimina valores mágicos relacionados a las respuestas de los comandos

--- src/utils/operationHoursUtils.js
+++ src/utils/operationHoursUtils.js
@@ -1,15 +1,16 @@
 import { replaceAt, fillLeftText } from "./syrusUtils";
-import { SYRUS4_SET_PARAMETER_KEYS, SYRUS_FOUR_COMMANDS } from "./constants";
+import { SYRUS4_SET_PARAMETER_KEYS, SYRUS_FOUR_COMMANDS, SYRUS3_MESSAGE_HEADERS } from "./constants";
 import { formatHour12, toGMT0, convertTo24h, toGMT5 } from "./timeUtils"
 
 /**
  * Procesa el mensaje de socket de un Syrus para obtener y formatear el horario de operación.
+ * Syrus -> Dispositivos inferiores a Syrus 4.
  * @param {string | null | undefined} message - El mensaje del socket a procesar.
  * @returns {string | null} Una cadena de texto con el horario de operación en formato local (ej: "08:00 a.m a 05:00 p.m") o null si no se pudo procesar.
  */
 export function generateOperationHours(messages) {
-    const qgt00 = messages['RGT001'];
-    const qgt01 = messages['RGT011'];
+    const qgt00 = messages[SYRUS3_MESSAGE_HEADERS.RES_CMD_QGT001];
+    const qgt01 = messages[SYRUS3_MESSAGE_HEADERS.RES_CMD_QGT011];
     if (!qgt00 || !qgt01) return "Parámetro inválido.";
     const response = qgt00.substring(1, 23).concat(qgt01.substring(23, qgt01.length - 1));
     const startTime = response.charAt(12).concat(response.charAt(13));

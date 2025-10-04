865e67fe696b53beba0bfc595c8576c0116ee0ad
Edison <softwaredev@totaltrackinggps.com>
2025-10-04 09:22:36 -0500

    style: elimina espaciado

--- src/pages/informationplant/components/AccumulatedPanel.jsx
+++ src/pages/informationplant/components/AccumulatedPanel.jsx
@@ -12,7 +12,6 @@ import LastMonthAccumulatedPanel from "../components/LastMonthAccumulatedPanel";
  */
 function AcummulatedPanel({ plant, isOnline }) {
     const mvZeroValue = useMemo(() => getMvZeroText(plant.info.description), [plant.info.description]);
-
     return (
         <div className="months-container flex flex-col border border-[#ccc] mb-4 p-0 overflow-y-auto">
             <HeaderPanel title={"Acumulados del mes actual y mes anterior"} />

73f31f306e139fb24dd4a28cb9e3afed711f89e9
Edison <softwaredev@totaltrackinggps.com>
2025-10-07 15:50:12 -0500

    feat: Encapsula la lógica relacionada a la obtención de información de la planta.

--- src/pages/informationplant/PlantDetailsPage.jsx
+++ src/pages/informationplant/PlantDetailsPage.jsx
@@ -1,13 +1,10 @@
 import DescriptionPanel from './components/DescriptionPanel';
 import OperationsPanel from './components/OperationsPanel';
 import AcummulatedPanel from './components/AccumulatedPanel';
-import { usePlants, useConnectionStatus } from "../../hooks/usePlants";
-import { useSyrus4Data } from '../../hooks/useSyrus4Data';
+import { usePlantInfo } from "../../hooks/usePlantInfo";
 import { PlantDetailSocketProvider } from '../../context/PlantDetailSocketContext';
 import { useParams } from 'react-router-dom';
-import { searchPlant } from '@/utils/syrusUtils';
 import StatusMessage from '@/components/StatusMessage';
-import { useEffect, useMemo } from 'react';
 /**
  * Página que muestra los detalles de una planta específica.
  * Obtiene la información de la planta, su estado de conexión y renderiza los paneles
@@ -16,35 +13,7 @@ import { useEffect, useMemo } from 'react';
  */
 function PlantDetailsPage() {
     const { idPlanta } = useParams();
-    const { plants, isLoading: loadingPlants } = usePlants();
-
-    // Busca la información de la planta una vez que la lista de plantas ha cargado.
-    const plant = !loadingPlants && Array.isArray(plants) ? searchPlant(plants, idPlanta) : null;
-
-    // Obtiene el estado de conexión del dispositivo asociado a la planta.
-    const { infoConnectionDevice, loading: loadingConnection, error: errorConnection } = useConnectionStatus(plant?.device);
-    //Determina si es syrus 4
-    const isSyrus4 = useMemo(
-        () => infoConnectionDevice?.version?.vkey_model?.toLowerCase().includes('syrus 4') || infoConnectionDevice?.version?.vkey_model?.toLowerCase().includes('s4') || infoConnectionDevice?.version?.vkey_model?.toLowerCase().includes('4g'),
-        [infoConnectionDevice]
-    );
-    // Llama al hook personalizado para la lógica de Syrus 4.
-    const { syrus4Data, isLoading: isLoadingSyrus4, fetchData: fetchSyrus4Data } = useSyrus4Data(plant, isSyrus4);
-
-    useEffect(() => {
-        const isOnline = infoConnectionDevice?.connection?.online ?? false;
-        //Este condicional evita la condición de carrera cuando se pasa de un s4 a un s3.
-        //Esto evita que se realicen consultas de S4 a APIS de S3.
-        if (plant?.device === infoConnectionDevice?.imei && !loadingConnection) {
-            if (plant && isOnline && isSyrus4) {
-                fetchSyrus4Data();
-            }
-        }
-    }, [plant, infoConnectionDevice, isSyrus4, loadingConnection, fetchSyrus4Data]);
-
-    // Define el estado de conexión en una variable para mayor claridad y reutilización.
-    const isOnline = infoConnectionDevice?.connection?.online ?? false;
-
+    const { plant, infoConnectionDevice, isOnline, isSyrus4, syrus4Data, isLoadingSyrus4, errorConnection, loadingPlants, loadingConnection } = usePlantInfo(idPlanta);
     return (
         <PlantDetailSocketProvider plantId={isOnline ? plant?.id : null} isOnline={isOnline}>
             <div className="info-container flex flex-col p-4 overflow-y-auto">
